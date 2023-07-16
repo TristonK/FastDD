a43440940e891087bae6d49872cabd7fb7154cef
shuleiKuang <923950043@qq.com>
2023-07-16 11:48:04 +0800

    feat:add dd-ie-hybrid-test

---
+++ src/main/java/ie/hybrid/Minimal.java
@@ -0,0 +1,13 @@
+package ie.hybrid;
+
+import ddfinder.differentialdependency.DifferentialDependency;
+import ddfinder.differentialdependency.DifferentialDependencySet;
+
+/**
+ * @author tristonK 2023/7/15
+ */
+public class Minimal {
+    public DifferentialDependencySet minimize(DifferentialDependencySet ddSet){
+
+    }
+}
