30545122c5025c1153e4d5bf9f489020236dedce
shuleiKuang <923950043@qq.com>
2023-07-16 17:22:11 +0800

    feat: add dd-ie test

--- src/main/java/ddfinder/differentialdependency/DifferentialDependency.java
+++ src/main/java/ddfinder/differentialdependency/DifferentialDependency.java
@@ -1,6 +1,7 @@
 package ddfinder.differentialdependency;
 
 import ch.javasoft.bitset.IBitSet;
+import ch.javasoft.bitset.LongBitSet;
 import ddfinder.predicate.DifferentialFunction;
 
 import java.util.List;
@@ -24,6 +25,10 @@ public class DifferentialDependency {
         this.right = right;
         this.predicateSet = predicateSet;
         this.leftPredicateSet = leftPredicates;
+    }
+
+    public DifferentialDependency(LongBitSet left, LongBitSet right){
+
     }
 
     public static final String AND = " ∧ ";
