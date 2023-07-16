2fd8b61536c25db0e586a3feb40d3c005584e48b
shuleiKuang <923950043@qq.com>
2023-07-16 11:48:04 +0800

    feat:add dd-ie-hybrid-test

---
+++ src/main/java/ie/hybrid/Analyzer.java
@@ -0,0 +1,20 @@
+package ie.hybrid;
+
+import ch.javasoft.bitset.LongBitSet;
+import ddfinder.differentialdependency.DifferentialDependencySet;
+import ddfinder.evidence.EvidenceSet;
+
+/**
+ * @author tristonK 2023/7/15
+ */
+public class Analyzer {
+    public void run(){
+        // for each predicate
+        // reduce
+        // minimize
+        // output
+    }
+    public DifferentialDependencySet reduce(EvidenceSet evidenceSet, LongBitSet right, LongBitSet dfSpace){
+
+    }
+}
