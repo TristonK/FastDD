e3b60002fed61b38aa2b39c4dc887a4e98038d30
shuleiKuang <923950043@qq.com>
2023-09-30 21:55:20 +0800

    chore: update config

--- src/main/java/fastdd/Config.java
+++ src/main/java/fastdd/Config.java
@@ -1,8 +1,5 @@
 package fastdd;
 
-/**
- * @author tristonK 2023/6/24
- */
 public class Config {
     public static int PliShardLength = 10000;
     public static boolean OutputDD2File = false;
@@ -10,7 +7,6 @@ public class Config {
     public static boolean OutputPredicateFlag = false;
     public static boolean OutputDFSet = false;
     public static boolean NeedRightAlwaysTrueDD = true;
-    public static boolean TestMD = false;
     public static boolean TestMultiThread = true;
-    public static int ThreadSize = 6;
+    public static int ThreadSize = 1;
 }
